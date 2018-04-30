0ddc5e21b9623bd46360d4596ee2108764d48e7f
Christopher Williams <chrwilliams@csumb.edu>
2018-04-30 15:22:27 -0700

    Added client-server audio data transfer via base64 string

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for, redirect, request, Response
+from flask import Flask, render_template, url_for, redirect, request, Response, jsonify
 from flask_bootstrap import Bootstrap
 from PIL import Image
 from werkzeug.utils import secure_filename
 import os
+import base64
 
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = set(['mp3','m4a'])
@@ -10,29 +11,22 @@ ALLOWED_EXTENSIONS = set(['mp3','m4a'])
 app= Flask(__name__, static_url_path = "/static", static_folder = "static")
 bootstrap = Bootstrap(app)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# CURRENT ROUTE WHERE FILE DATA IS SENT
+@app.route('/test', methods=['POST','GET'])
+def test_audio():
+    jsonData = request.get_json()
 
-def read_file(file):
-    return file.read()
+    # THIS IS THE AUDIO BYTE DATA (The File)
+    decoded = base64.b64decode(jsonData['data'])
+
+    # Print to console the audio byte data
+    print(decoded)
+
+    # For now return the unmodified base64 string data sent up
+    return jsonify(jsonData['data'])
+    # return jsonify("Got Data")
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            return render_template('uploaded_file.html', file=str(file.mimetype))
     return render_template('index.html')
